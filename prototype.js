b48442b8146cfcd8f9638884cc65fdaa38ce387c
MST <bilkis.naher@yahoo.co.uk>
2025-01-19 12:23:11 +0000

    add OOP's prototype files

---
+++ prototype.js
@@ -0,0 +1,19 @@
+//  1.
+myArray = [2, 4, 5, 6, 7];
+console.log(myArray);
+myArray.forEach((num) => console.log(num));
+myArray.map((x) => console.log(x*2));
+
+//2.
+myObject = {
+    name: "Samantha",
+    age: 25,
+    occupation: "Curriculum Dev"
+};
+
+console.log(myObject);
+
+console.log("Hello");
+console.log("Hello".toLowerCase());
+console.log(546234);
+console.log((546234).toString());
